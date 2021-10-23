9c8f43630a7e2462827bdad1b60eb15ec5880f96
simonachess <simonachess@gmail.com>
2021-10-23 17:20:24 +0300

    unlock button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
 
-function App() {
+export default function App() {
+
+  function Button({ isLocked, children, ...other }) {
+    const className = isLocked ? "button button--locked" : "button";
+    return (
+      <button class={className} disabled={isLocked} {...other}>
+        {isLocked ? "Oh no! I'm locked :(" : children}
+      </button>
+    );
+  }
+
+  const [isLocked, setIsLocked] = useState(true);
+  const unlockSecondButton = () => {
+    // unlock second button on click
+    console.log('Click happened')
+    setIsLocked(!isLocked);
+  };
+
+  const [countdown, setCountdown] = useState(5)
+  const [isLocked2, setIsLocked2] = useState(true);
+  const unlockThirdButton = () => {
+    // unlock third button after 5 clicks
+    if (countdown > 1) {
+      setCountdown(countdown - 1)
+    } else {
+      setIsLocked2(!isLocked2)
+    }
+
+  };
+  let word = "clikcs"
+  if (countdown === 1) {
+    word = "click"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-containter">
+      <div className="button-wrapper">
+        <Button onClick={unlockSecondButton}>
+          I will unlock Second Button on click
+        </Button></div>
+      <div className="button-wrapper">
+        <Button onClick={unlockThirdButton} isLocked={isLocked}>
+          I will unlock Third Button after {countdown} {word}
+        </Button></div>
+      <div className="button-wrapper">
+        <Button isLocked={isLocked2}>Yay I'm free! :)</Button>
+      </div>
     </div>
   );
 }
-
-export default App;
